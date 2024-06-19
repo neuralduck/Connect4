1111dcc2f668b3c58045fe73e4e85681c19d396b
neuralduck <135601034+neuralduck@users.noreply.github.com>
2024-06-20 00:37:51 +0530

    basic game complete.
    could use some tweaks here and there

---
+++ connect4.py
@@ -0,0 +1,158 @@
+import os
+
+class ConnectFour:
+	def __init__(self):
+		self.board = [[0 for _ in range(7)] for _ in range(6)]
+		self.symbol1 = '\N{WHITE CIRCLE}'
+		self.symbol2 = '\N{BLACK CIRCLE}'
+		self.move_count = {i: 0 for i in range(7)}
+	def __str__(self):
+		board_str = ''
+		get_symbol = lambda x: f'{self.symbol1}' if x == 1 else f'{self.symbol2}' if x == -1 else ' '
+		dash = '\N{Horizontal Bar}'
+		board_str += '| '+' | '.join(list(map(str, range(7))))+' |'+'\n'
+		board_str += '-'*29+'\n'
+		for row in self.board:
+			board_str += '| '+' | '.join(list(map(get_symbol, row)))+' |'+'\n'
+			board_str += '-'*29+'\n'
+		return board_str
+
+	def move(self, column, mark):
+		if self.move_count[column] < 6:
+			for cell in list(range(6))[::-1]:
+				if self.board[cell][column] == 0:
+					self.board[cell][column] = mark
+					self.move_count[column] += 1
+					break
+
+	def available_moves(self):
+		#returns a list of column numbers with empty cells
+		return list(map(lambda x: x[0], filter(lambda x: True if x[1] < 6 else False, self.move_count.items())))
+	
+	def num_cells(self):
+		#number of empty cells in the board, for draw and game over checks
+		return 42 - sum(self.move_count.values())
+
+	def check(self, index = False):
+		# +1 player 1 | -1 player 2 | 0 draw | 3 game not over
+		if self.num_cells() == 0:
+			#draw
+			if index:
+				return [0, None, None]
+			return 0
+		for i in range(6):
+			for j in range(7):
+				if abs(self.board[i][j]) == 1:
+					RIGHT = (j+3 <= 6)
+					LEFT = (j-3 >= 0)
+					UP = (i-3 >= 0)
+					DOWN = (i+3 <= 5)
+					if RIGHT:
+						right = 0
+						for n in range(4):
+							right += self.board[i][j+n]
+						if abs(right) == 4:
+							if index:
+								return [int(right/4),(i, j), (i, j+3)]
+							return int(right/4)
+						if UP:
+							right_up = 0
+							for n in range(4):
+								right_up += self.board[i-n][j+n]
+							if abs(right_up) == 4:
+								if index:
+									return [int(right_up/4), (i, j), (i-3, j+3)]
+								return int(right_up/4)
+						if DOWN:
+							right_down = 0
+							for n in range(4):
+								right_down += self.board[i+n][j+n]
+							if abs(right_down) == 4:
+								if index:
+									return [int(right_down/4), (i, j), (i+3, j+3)]
+								return int(right_down/4)
+					if LEFT:
+						left = 0
+						for n in range(4):
+							left += self.board[i][j-n]
+						if abs(left) == 4:
+							if index:
+									return [int(left/4), (i, j), (i, j-3)]
+							return int(left/4)
+
+						if UP:
+							left_up = 0
+							for n in range(4):
+								left_up += self.board[i-n][j-n]
+							if abs(left_up) == 4:
+								if index:
+									return [int(left_up/4), (i, j), (i-3, j-3)]
+								return int(left_up/4)
+
+						if DOWN:
+							left_down = 0
+							for n in range(4):
+								left_down += self.board[i+n][j-n]
+							if abs(left_down) == 4:
+								if index:
+									return [int(left_down/4), (i, j), (i+3, j-3)]
+								return int(left_down/4)
+					if UP:
+						up = 0
+						for n in range(4):
+							up += self.board[i-n][j]
+						if abs(up) == 4:
+							if index:
+									return [int(up/4), (i, j), (i-3, j)]
+							return int(up/4)
+
+					if DOWN:
+						down = 0
+						for n in range(4):
+							down += self.board[i+n][j]
+						if abs(down) == 4:
+							if index:
+									return [int(down/4), (i, j), (i+3, j)]
+							return int(down/4)
+		if index:
+			return [3, None, None]
+		return 3
+
+if __name__ == '__main__':
+	def info():
+		os.system('clear||cls')
+		print(board)
+	turn = 1
+	board = ConnectFour()
+	player1 = 1
+	player2 = -1
+	while True:
+		info()
+		if turn:
+			choice = int(input(f"Player 1 choice ({board.symbol1}): "))
+		else:
+			choice = int(input(f"Player 2 choice ({board.symbol2}): "))
+		if board.move_count[choice] == 6:
+			print('column full')
+			continue
+		if choice not in range(7):
+			print('pick a choice within range')
+			continue
+		if turn:
+			board.move(choice, player1)
+		else:
+			board.move(choice, player2)
+		turn = int(not(turn))
+		result = board.check()
+		if result == 1:
+			info()
+			print(f'Player 1 ({board.symbol1}) wins')
+			break
+		elif result == -1:
+			info()
+			print(f'Player 2 ({board.symbol2}) wins')
+			break
+		elif result == 0:
+			info()
+			print('Draw')
+			break
---
+++ game.py
@@ -0,0 +1,128 @@
+import pygame as pg 
+from connect4 import ConnectFour
+class Ball:
+	def __init__(self, x_pos, y_pos, color):
+		#Ball(50, 0, 50, 'blue', 0.5, 0, 0, -1)
+		self.x_pos = x_pos
+		self.y_pos = y_pos
+		self.color = color
+		self.radius = 50
+		self.retention =  0.08
+		self.y_speed = 0
+		self.circle = ''
+		self.gravity = 3
+		self.bounce_stop = 0.4
+	def draw(self, screen):
+		self.circle = pg.draw.circle(screen, self.color, (self.x_pos, self.y_pos), self.radius)
+	def update_pos(self, stack_height = 0):
+		height = 600
+		if self.y_pos < height - self.radius - (stack_height*self.radius*2):
+			self.y_speed += self.gravity
+		elif self.y_speed > self.bounce_stop:
+				self.y_speed = self.y_speed * -1 * self.retention
+		elif abs(self.y_speed) <= self.bounce_stop:
+				self.y_speed = 0
+		self.y_pos += self.y_speed
+
+class Game:
+	def __init__(self):
+
+		self.board = ConnectFour()
+
+		self.grid2board = lambda x: int((x-50)/100)
+		self.board2grid = lambda x: int((x*100)+50)
+		self.p1_color = ()
+		self.p2_color = ()
+		self.on_screen = {50: [], 150: [], 250: [], 350: [], 450: [], 550: [], 650: []}
+		self.last_played = None
+		self.turn = 0
+		self.game_over = False
+		
+		pg.init()
+		self.running = True
+		self.width = 700
+		self.height = 600
+		self.screen = pg.display.set_mode((self.width, self.height))
+		self.fps = 60
+		self.timer = pg.time.Clock()
+
+	def draw_walls(self):
+		self.p1_color = (7, 183, 38, 0.76)
+		self.p2_color = (119, 3, 179, 0.76)
+		self.screen.fill((165, 221, 227, 0.45))
+		even = False
+		for row in range(0, self.width, 100):
+			for col in range(0, self.height, 100):
+				if even:
+					pg.draw.rect(self.screen, (140, 200, 220, 0.45), ((row, col), (100, 100)))
+				even = not(even)
+			even = not(even)
+		for x in range(0, self.width+100, 100):
+			wall = pg.draw.line(self.screen, 'white', (x, 0), (x, self.height), 3)
+		for y in range(0, self.height+100, 100):
+			pg.draw.line(self.screen, 'white', (0, y), (self.width, y), 3)
+
+	def classic_board(self):
+		self.p1_color = 'red'
+		self.p2_color = 'yellow'
+		self.screen.fill((0, 0, 255))
+		for x in range(0, self.width, 100):
+			for y in range(0, self.height, 100):
+				x = 100*(x//100)+50
+				y = 100*(y//100)+50
+				pg.draw.circle(self.screen, 'black', (x, y), 48)
+
+	def play(self):
+		while self.running:
+			self.timer.tick(self.fps)
+			self.draw_walls()
+			#self.classic_board()
+			for col in self.on_screen:
+				for n, ball in enumerate(self.on_screen[col]):
+					ball.draw(self.screen)
+					ball.update_pos(n)
+			result, start, end = self.board.check(True)
+			print(result, start, end)
+			if not self.game_over:
+				if result == 1:
+					print('Player 1 wins')
+					self.game_over = True
+				elif result == -1:
+					print('Player 2 wins')
+					self.game_over = True
+				elif result == 0:
+					print('Draw')
+					self.game_over = True
+			else:
+				print('game over')
+				if (start != None) and (end != None):
+					start = list(map(self.board2grid, start))[::-1]
+					end = list(map(self.board2grid, end))[::-1]
+					if self.p1_color == 'red':
+						pg.draw.line(self.screen, 'green', start, end, 8)
+					else:
+						pg.draw.line(self.screen, 'black', start, end, 5)
+
+			for event in pg.event.get():
+				if event.type == pg.QUIT:
+					self.running = False
+				if (event.type == pg.MOUSEBUTTONUP) and not(self.game_over):
+					x, _ = pg.mouse.get_pos()
+					x = 100*(x//100)+(50)
+					#print(x)
+					self.last_played = x
+					if len(self.on_screen[x]) < 6:
+						if self.turn:
+							self.on_screen[x].append(Ball(x, 0, self.p2_color))
+							self.board.move(int((x-50)/100), -1)
+						else:
+							self.on_screen[x].append(Ball(x, 0, self.p1_color))
+							self.board.move(self.grid2board(x), 1)
+						self.turn = int(not(self.turn))
+			pg.display.update()
+			#print(self.board)
+		pg.quit()
+
+if __name__ == '__main__':
+	game = Game()
+	game.play()
